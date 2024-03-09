f6533b6e482747c746ec1804f94740f30dd3d1ad
Roald Ruiter <roaldruiter@gmail.com>
2024-03-09 20:45:08 +0100

    prompt text areas automatically resize vertically

---
+++ ChatGptDiffApp/wwwroot/textareaAutoResize.js
@@ -0,0 +1,18 @@
+// Source: https://stackoverflow.com/a/25621277/6329629
+window.textareaAutoResize = {
+  initialize: function () {
+    const tx = document.getElementsByTagName("textarea");
+    for (let i = 0; i < tx.length; i++) {
+      tx[i].setAttribute(
+        "style",
+        "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;",
+      );
+      tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput() {
+      this.style.height = "auto";
+      this.style.height = this.scrollHeight + "px";
+    }
+  },
+};
